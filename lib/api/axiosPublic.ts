2637bf197d4e8dafc18aa2fe52e0f2da60919185
Pranshu Verma <pranshuverma1601@gmail.com>
2025-09-02 17:37:16 +0530

    fix bug

--- lib/api/axiosPublic.ts
+++ lib/api/axiosPublic.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://192.168.29.41:8000/api/v1";
+// const API_URL = "http://192.168.29.41:8000/api/v1";
+const API_URL = "https://hospital-navigation-backend.onrender.com/api/v1";
 
 export const axiosPublic = axios.create({
   baseURL: API_URL,
